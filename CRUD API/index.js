d238c3a17a0ba4b56b423981578164475dcc07a9
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2025-05-25 23:14:52 +0530

    Post method product created

--- CRUD API/index.js
+++ CRUD API/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
 
+const app = express();
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Hello World! from Node API.");
+})
+
+app.post('/addProduct', async (req, res) => {
+    try{
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
 })
 
 mongoose.connect("mongodb+srv://admin:vsWCCOXZijZaECtx@backenddb.xaqkvlp.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")

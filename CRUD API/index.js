035ba53bdf10334aa0175cecded38c6d0ad112c3
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2025-05-25 19:32:06 +0530

    MongoDB database connected

--- CRUD API/index.js
+++ CRUD API/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-app.listen(3000,()=>{
-    console.log("Server started on port 3000!");
-});
+
 
 app.get('/', (req, res) => {
     res.send("Hello World! from Node API.");
 })
+
+mongoose.connect("mongodb+srv://admin:vsWCCOXZijZaECtx@backenddb.xaqkvlp.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
+    .then(() => {
+        console.log("Database Connected!");
+        app.listen(3000,()=>{
+            console.log("Server started on port 3000!");
+        });
+    })
+    .catch((err) => {
+        console.log("Connection failed. "+err);
+    });
